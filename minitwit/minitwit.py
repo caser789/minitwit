ea19fa965a0a61d369b35c27ccef297ee5be66f0
xuejiao <jiao.xuejiao@gmail.com>
2016-08-28 15:32:04 +0800

    implement get_db

--- minitwit/minitwit.py
+++ minitwit/minitwit.py
@@ -1,6 +1,25 @@
-from flask import Flask
+import os
+from flask import Flask, _app_ctx_stack
+from sqlite3 import dbapi2 as sqlite3
 
 app = Flask(__name__)
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, '/tmp/minitwit.db'),
+    DEBUG=True,
+    SECRET_KEY='xuejiao',
+    PER_PAGE=30,
+    ))
+# app.config.from_object(__name__)
+app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
+
+def get_db():
+    """open a new db connection if no one for current app context"""
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        top.sqlite_db.row_factory = sqlite3.Row
+    return top.sqlite_db
+
 
 if __name__ == '__main__':
     app.run(debug=True)
