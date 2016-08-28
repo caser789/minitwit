4f12f9eb6acafc36f5dcc2c11be82a461a57b226
xuejiao <jiao.xuejiao@gmail.com>
2016-08-28 15:34:22 +0800

    add init_db

--- minitwit/minitwit.py
+++ minitwit/minitwit.py
@@ -20,6 +20,15 @@ def get_db():
         top.sqlite_db.row_factory = sqlite3.Row
     return top.sqlite_db
 
+def init_db():
+    """initialize the db"""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
